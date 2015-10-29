a1c7010f6aee222f109558eed4f402e197bbf254
Charles Ellis <hamled@hamled.name>
2015-10-29 15:50:25 -0700

    Add example route with partial and erb layout file

--- space_jam.rb
+++ space_jam.rb
@@ -3,8 +3,15 @@ require "sinatra"
 class SpaceJam < Sinatra::Base
 
   get "/" do
+    @title = "Space Jam"
     # Take them to the Space Jam
     erb :jam
+  end
+
+  get "/example" do
+    @title = "Example"
+    @cat_names = ["Princess Caroline", "Catmando", "Wilberforce"]
+    erb :example
   end
 
 end
